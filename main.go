06bf2b9b68ac992ca165da7c7030362b1f2d6d70
CodyJReed <cody2j2reed@gmail.com>
2019-10-23 00:48:26 -0400

    Completed excercise 4

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import "fmt"
 
-var x int = 42
+var w int = 42
 var y string = "James Bond"
 var z bool = true
+type ownType int
+var x ownType
 
 func main() {
 
 	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
 	fmt.Println(s)
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
 }
